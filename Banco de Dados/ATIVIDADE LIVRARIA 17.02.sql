0d64f1799d4f75eb7af539b3933caa2b3b521426
Monicamkvb <monicakarolbraga@gmail.com>
2025-02-17 17:39:51 -0300

    ATIVIDADE 17.02

--- Banco de Dados/ATIVIDADE LIVRARIA 17.02.sql
+++ Banco de Dados/ATIVIDADE LIVRARIA 17.02.sql
@@ -10,8 +10,8 @@ nacionalidade VARCHAR(100)
 CREATE TABLE livros(
 id INT PRIMARY KEY AUTO_INCREMENT,
 titulo VARCHAR(100) NOT NULL,
-ano_publicacao INT,
-id_autor INT,
+ano_publicacao YEAR,
+id_autor INT NOT NULL,
 FOREIGN KEY (id_autor) REFERENCES autores(id_autor)
 );
 
@@ -27,10 +27,24 @@ INSERT INTO livros (titulo, ano_publicacao, id_autor) VALUES ("Norwegian wood", 
 
 SELECT * FROM autores;
 SELECT * FROM livros;
-SELECT Livros.titulo , Autores.nome FROM Livros JOIN livros_autores
-ON Livros.id_livro = livros_autores.id_livro
-JOIN Autores ON livros_autores.id_autor = Autores.id_autor;
 
+CREATE TABLE livros_autores(
+id_autor INT NOT NULL,
+id_livro INT NOT NULL,
+FOREIGN KEY (id_autor) REFERENCES autores(id_autor),
+FOREIGN KEY (id_autor) REFERENCES autores(id_autor)
+);
 
+SELECT * FROM livros_autores;
 
+INSERT INTO livros_autores(id_autor,id_livro) VALUES (1,1);
+INSERT INTO livros_autores(id_autor,id_livro) VALUES (1,2);
+INSERT INTO livros_autores(id_autor,id_livro) VALUES (2,3);
+INSERT INTO livros_autores(id_autor,id_livro) VALUES (2,4);
+INSERT INTO livros_autores(id_autor,id_livro) VALUES (3,5);
+
+-- TABELA.COLUNA
+SELECT livros.titulo , autores.nome, livros.ano_publicacao, autores.nacionalidade FROM livros JOIN livros_autores
+ON livros.id = livros_autores.id_livro
+JOIN autores ON livros_autores.id_autor = autores.id_autor
 
