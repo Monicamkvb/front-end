11f4e0c7bb9fd61b4f4d34b1116729090e5a30a4
Monicamkvb <monicakarolbraga@gmail.com>
2025-03-11 17:46:19 -0300

    Jogos

--- BackEnd/jogos/index.php
+++ BackEnd/jogos/index.php
@@ -6,6 +6,24 @@
     <title>Login</title>
 </head>
 <body>
-    <a href="assets/pages/home.php">home</a>
+
+    <main>
+        <form action="assets/scripts/login.php" method="post">
+            <!-- email -->
+             <div class="form-group">
+                <label for="email">Email: </label>
+                <input placeholder="Digite um email 'jhondoe@gmail.com'" type="email" required name="email" id="email">
+             </div>
+             <!-- senha -->
+             <div class="form-group">
+                <label for="senha">Senha: </label>
+                <input placeholder="Digite sua senha" type="password" required name="senha" id="senha">
+             </div>
+             <button type="submit">LOGIN</button>
+             <p>Esqueceu a senha? <a href="#">Clique aqui</a></p>
+             <p>Não possui conta? <a href="assets/pages/cadastrar.php">Cadastre-se</a></p>
+        </form>
+    </main>
+
 </body>
 </html>
